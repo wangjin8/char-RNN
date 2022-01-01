51ea190597f52dd57c2a2c27249cffc040839c22
wang <1553721524@qq.com>
2022-01-01 12:31:22 +0800

    third

--- sample.py
+++ sample.py
@@ -11,14 +11,14 @@ tf.flags.DEFINE_integer('num_layers', 2, 'number of lstm layers')
 tf.flags.DEFINE_boolean('use_embedding', False, 'whether to use embedding')
 tf.flags.DEFINE_integer('embedding_size', 128, 'size of embedding')
 tf.flags.DEFINE_string('converter_path', 'model1/default/converter.pkl', 'model/default/converter.pkl')
-tf.flags.DEFINE_string('checkpoint_path', 'model1/default/model-1000', 'model/default/')
+tf.flags.DEFINE_string('checkpoint_path', 'model1/default/', 'model/default/')
 tf.flags.DEFINE_string('start_string', '', 'use this string to start generating')
-tf.flags.DEFINE_integer('max_length', 3000, 'max length to generate')
+tf.flags.DEFINE_integer('max_length', 50, 'max length to generate')
 
 # FLAGS.converter_path='model1/default/converter.pkl'
 # FLAGS.checkpoint_path='model1/default/'
 # FLAGS.max_length=1000
-
+filename = "data/3.txt"
 def main(_):
     FLAGS.start_string = FLAGS.start_string.encode('utf-8').decode('utf-8')
     converter = TextConverter(filename=FLAGS.converter_path)
@@ -36,6 +36,9 @@ def main(_):
     start = converter.text_to_arr(FLAGS.start_string)
     arr = model.sample(FLAGS.max_length, start, converter.vocab_size)
     print(converter.arr_to_text(arr))
+    # with open(filename, 'w+',encoding="utf-8") as f: # 中英文encoding必须加参数
+    with open(filename, 'w+') as f:
+        f.write(converter.arr_to_text(arr))
 
 
 if __name__ == '__main__':
